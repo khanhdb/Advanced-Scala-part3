ed3d4fe4b8346c524ee2d5156ed1e5d68575e39f
Bao Khanh Dinh <khanhdb@sandinh.net>
2021-05-07 06:49:03 +0700

    add solution

--- src/main/scala/FridaySet.scala
+++ src/main/scala/FridaySet.scala
@@ -1,4 +1,3 @@
-
 trait FridaySet[A] extends (A => Boolean){
   def size : Int
   def isEmpty: Boolean
@@ -11,33 +10,37 @@ trait FridaySet[A] extends (A => Boolean){
 }
 
 case class EmptySet[A]() extends FridaySet[A]{
-  override def size: Int = ???
+  override def size: Int = 0
 
-  override def isEmpty: Boolean = ???
+  override def contains(x: A): Boolean = false
 
-  override def contains(x: A): Boolean = ???
+  override def +(x: A): FridaySet[A] = NoneEmptySet(x, this)
 
-  override def +(x: A): FridaySet[A] = ???
+  override def ++(xs: FridaySet[A]): FridaySet[A] = xs
 
-  override def ++(xs: FridaySet[A]): FridaySet[A] = ???
+  override def map[B](f: A => B): FridaySet[B] = EmptySet[B]()
 
-  override def map[B](f: A => B): FridaySet[B] = ???
+  override def foreach(f: A => Unit): Unit = ()
 
-  override def foreach(f: A => Unit): Unit = ???
+  override def isEmpty: Boolean = true
+
 }
 
 case class NoneEmptySet[A](head: A, tail: FridaySet[A]) extends FridaySet[A] {
-  override def size: Int = ???
+  override def size: Int = tail.size + 1
 
-  override def isEmpty: Boolean = ???
+  override def isEmpty: Boolean = false
 
-  override def contains(x: A): Boolean = ???
+  override def contains(x: A): Boolean = head == x || this.tail.contains(x)
 
-  override def +(x: A): FridaySet[A] = ???
+  override def +(x: A): FridaySet[A] = NoneEmptySet(x, this)
 
-  override def ++(xs: FridaySet[A]): FridaySet[A] = ???
+  override def ++(xs: FridaySet[A]): FridaySet[A] = (xs ++ this.tail) + this.head
 
-  override def map[B](f: A => B): FridaySet[B] = ???
+  override def map[B](f: A => B): FridaySet[B] = this.tail.map(f) + f(this.head)
 
-  override def foreach(f: A => Unit): Unit = ???
+  override def foreach(f: A => Unit): Unit = {
+    f(this.head)
+    this.tail.foreach(f)
+  }
 }
